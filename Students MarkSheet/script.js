4c247fec65d344218f24b6197b67a91cb7222680
coder-BS <105738925+coder-BS@users.noreply.github.com>
2023-07-10 22:52:40 +0530

    First commit

---
+++ Students MarkSheet/script.js
@@ -0,0 +1,8 @@
+function calculate() {
+    const name = document.getElementById("name").value;
+    const subject1 = parseFloat(document.getElementById("subject1").value);
+    const subject2 = parseFloat(document.getElementById("subject2").value);
+    const subject3 = parseFloat(document.getElementById("subject3").value);
+    const subject4 = parseFloat(document.getElementById("subject4").value);
+
+    const totalMarks = subject1
